db04dd0ec1c71821c2f66cece86647b4669f6443
Lucas Sulzbach rilho <lsr.jupi@gmail.com>
2024-02-06 20:00:53 +0100

    mod 00 ok?

--- module_00/ex01/main.cpp
+++ module_00/ex01/main.cpp
@@ -4,26 +4,23 @@
 #include "Contact.hpp"
 #include "Phonebook.hpp"
 
-int main()
+int main(void)
 {
     Phonebook pb;
     bool isOn = true;
     std::string input;
-//     Contact c;
-//     std::string info[] = {"Lucas", "ABCDEFGHIJK", "Lukita", "123456789", "Gosta de censurar as pessoas"};
-//     c.updateContact(info);
-//     pb.searchContact();
-// //    pb.addContact();
 
-//     return 0;
-    
     std::cout << "Welcome to your phonebook!\n";
     while (isOn)
     {
         std::cout << "Current contacts: " << pb.getSize() << "!\n";
         std::cout << "What do you want to do? ADD, SEARCH or EXIT.\n>>> ";
-        std::getline(std::cin, input);
-        if (input == "EXIT")
+        if (!std::getline(std::cin, input))
+        {
+            std::cout << "\nEOF found...\nExiting..\n";
+            isOn = false;
+        }
+        else if (input == "EXIT")
             isOn = false;
         else if (input == "SEARCH")
             pb.searchContact();
@@ -32,7 +29,6 @@ int main()
         else
             std::cout << "Cannot understand: " << input << "!\n";
     }
-
     return 0;
 }
 
---
+++ module_00/ex02/Account.cpp
@@ -0,0 +1,107 @@
+#include "Account.hpp"
+#include <iostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+int	Account::getNbAccounts( void )
+{
+	return _nbAccounts;
+};
+int	Account::getTotalAmount( void )
+{
+	return _totalAmount;
+};
+int	Account::getNbDeposits( void )
+{
+	return _totalNbDeposits;
+};
+int	Account::getNbWithdrawals( void )
+{
+	return _totalNbWithdrawals;
+};
+void	Account::displayAccountsInfos( void )
+{
+	if (_nbAccounts == 0)
+		return ;
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";";
+	std::cout << "total:" << getTotalAmount() << ";";
+	std::cout << "deposits:" << getNbDeposits() << ";";
+	std::cout << "withdrawals:" << getNbWithdrawals() << std::endl;
+};
+
+Account::Account( int initial_deposit )
+{
+	_accountIndex = _nbAccounts++;
+	_amount = initial_deposit;
+	_totalAmount += initial_deposit;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";created" << std::endl;
+};
+Account::~Account( void )
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";closed" << std::endl;
+};
+
+void	Account::makeDeposit( int deposit )
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "p_amount:" << _amount << ";";
+	std::cout << "deposit:" << deposit << ";";
+	_amount += deposit;
+	_totalAmount += deposit;
+	_nbDeposits++;
+	_totalNbDeposits++;
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "nb_deposits:" << _nbDeposits << std::endl;	
+};
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "p_amount:" << _amount << ";";
+	std::cout << "withdrawal:";
+	if (withdrawal > checkAmount())
+		return (std::cout << "refused\n", false);
+	std::cout << withdrawal << ";";
+	_amount -= withdrawal;
+	_totalAmount -= withdrawal;
+	_nbWithdrawals++;
+	_totalNbWithdrawals++;
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "nb_withdrawals:" << _nbWithdrawals << std::endl;	
+	return true;
+};
+int		Account::checkAmount( void ) const
+{
+	return _amount;
+};
+void	Account::displayStatus( void ) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "deposits:" << _nbDeposits << ";";
+	std::cout << "withdrawals:" << _nbWithdrawals << std::endl;	
+
+};
+
+void	Account::_displayTimestamp( void )
+{
+	std::time_t now = std::time(nullptr);
+    char buffer[80];
+    std::strftime(buffer, sizeof(buffer), "[%Y%m%d_%H%M%S] ", std::localtime(&now));
+    std::cout << buffer;
+}
+
+Account::Account( void )
+{
+	_accountIndex = _nbAccounts++;
+};
