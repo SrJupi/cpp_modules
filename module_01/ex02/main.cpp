90fd1ce47bb5e95326ad45422254d02f932b36dc
Lucas Sulzbach rilho <lsr.jupi@gmail.com>
2024-02-07 10:41:56 +0100

    mod01 git

--- module_01/ex02/main.cpp
+++ module_01/ex02/main.cpp
@@ -7,9 +7,8 @@ int main()
     std::string *stringPTR = &s;
     std::string &stringREF = s;
     
-    std::cout<<&s<< " -> " << s << std::endl;
-    std::cout<<&stringPTR<< " -> " << *stringPTR <<std::endl;
-    std::cout<<&stringREF<< " -> " << stringREF <<std::endl;
-
+    std::cout<< "String:    " <<  &s << " -> " << s << std::endl;
+    std::cout<< "StringPtr: " << &stringPTR << " -> " << *stringPTR <<std::endl;
+    std::cout<< "StringRef: " << &stringREF << " -> " << stringREF <<std::endl;
     return 0;
 }
