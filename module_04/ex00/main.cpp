9fe05e7cf48e973afe50b58bf4fe81a227418b9a
Lucas Sulzbach rilho <lsr.jupi@gmail.com>
2024-02-20 17:20:22 +0100

    mod04 ex00

---
+++ module_04/ex00/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Animal.hpp"
+#include "WrongAnimal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "WrongCat.hpp"
+
+int main( ) {
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	std::cout << meta->getType() << " " << std::endl;
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+
+	Animal k(*j);
+	std::cout << k.getType() << " " << std::endl;
+	k.makeSound();
+
+	delete meta;
+	delete j;
+	delete i;
+
+	std::cout << "\n\nCreating WrongAnimals... why? Don't know..." << std::endl;
+	WrongAnimal a;
+	std::cout << a.getType() << " " << std::endl;
+	a.makeSound();
+	WrongCat c;
+	std::cout << c.getType() << " " << std::endl;
+	c.makeSound();
+
+	return 0;
+}
