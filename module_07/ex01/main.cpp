134d85c4a9b125cded875aaabd2c6b98c7d0adbc
mint kinhas <lsr.jupi@gmail.com>
2024-05-24 13:33:49 +0200

    mod07 ex01

---
+++ module_07/ex01/main.cpp
@@ -0,0 +1,46 @@
+#include "iter.hpp"
+#include <iostream>
+
+template <typename T>
+void printElement(const T& element) {
+    std::cout << element << " ";
+}
+
+template <typename T>
+void doubleElement(T& element) {
+    element *= 2;
+}
+
+int main() {
+    int intArray[] = {1, 2, 3, 4, 5};
+    double doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    std::string stringArray[] = {"apple", "banana", "orange", "grape", "kiwi"};
+
+    std::cout << "Printing intArray:" << std::endl;
+    iter(intArray, 5, printElement<int>);
+    std::cout << std::endl;
+
+    std::cout << "Printing doubleArray:" << std::endl;
+    iter(doubleArray, 5, printElement<double>);
+    std::cout << std::endl;
+
+    std::cout << "Printing stringArray:" << std::endl;
+    iter(stringArray, 5, printElement<std::string>);
+    std::cout << std::endl;
+
+    std::cout << "Double intArray:" << std::endl;
+    iter(intArray, 5, doubleElement<int>);
+
+    std::cout << "Double doubleArray:" << std::endl;
+    iter(doubleArray, 5, doubleElement<double>);
+
+    std::cout << "Printing intArray:" << std::endl;
+    iter(intArray, 5, printElement<int>);
+    std::cout << std::endl;
+
+    std::cout << "Printing doubleArray:" << std::endl;
+    iter(doubleArray, 5, printElement<double>);
+    std::cout << std::endl;
+
+    return 0;
+}
